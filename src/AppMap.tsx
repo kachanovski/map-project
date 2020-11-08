87e8a505dca903dba41e76ebacf86211162a90c5
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-11-08 17:53:59 +0300

    first step

--- src/AppMap.tsx
+++ src/AppMap.tsx
@@ -1,135 +1,131 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { usePosition } from "use-position";
-import s from "./App.module.css";
-import { GeolocationControl, Map, Placemark, YMaps } from "react-yandex-maps";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  centerPositionAC,
-  FeaturesType,
-  getPlacemarksTC,
-  myLocationAC
-} from "./store/MapReducer";
-import { StateType } from "./store/store";
+import React, {ChangeEvent, useEffect, useState} from "react";
+import {usePosition} from 'use-position';
+import s from './App.module.css'
+import {GeolocationControl, Map, Placemark, YMaps} from "react-yandex-maps";
+import {useDispatch, useSelector} from "react-redux";
+import {centerPositionAC, FeaturesType, getPlacemarksTC, myLocationAC} from "./store/MapReducer";
+import {StateType} from "./store/store";
 import SearchBarInMap from "./components/SearchBarInMap";
 import { points } from "./App";
 
+
 const MapApp = () => {
-  const dispatch = useDispatch();
-  const feature = useSelector<StateType, Array<FeaturesType>>(
-    state => state.map.feature
-  );
-  const myLocation = useSelector<StateType, Array<number>>(
-    state => state.map.myLocation
-  );
-  const center = useSelector<StateType, Array<number>>(
-    state => state.map.center
-  );
-  const zoom = useSelector<StateType, number>(state => state.map.zoom);
-  const search = useSelector<StateType, string>(state => state.map.search);
 
-  const [searchValue, setSearchValue] = useState("");
+    const [hideResults, setHideResults] = useState(true)
+    const dispatch = useDispatch()
+    const feature = useSelector<StateType, Array<FeaturesType>>(state => state.map.feature)
+    const myLocation = useSelector<StateType, Array<number>>(state => state.map.myLocation)
+    const center = useSelector<StateType, Array<number>>(state => state.map.center)
+    const zoom = useSelector<StateType, number>(state => state.map.zoom)
+    const search = useSelector<StateType, string>(state => state.map.search)
 
-  const watch = true;
-  const { latitude, longitude } = usePosition(watch);
+    const [searchValue, setSearchValue] = useState('')
 
-  const mapState = {
-    width: 3538,
-    center: center,
-    zoom: zoom,
-    controls: []
-  };
+    const watch = true;
+    const {
+        latitude,
+        longitude,
+    } = usePosition(watch);
 
-  useEffect(
-    () => {
-      dispatch(getPlacemarksTC(searchValue));
-    },
-    [dispatch, searchValue]
-  );
-  const handleMyLocation = () => {
-    const location = [latitude!, longitude!];
-    dispatch(myLocationAC(location, 14));
-    dispatch(centerPositionAC(location));
-  };
+    const mapState = {
+        width: 3538,
+        center: center,
+        zoom: zoom,
+        controls: []
+    }
 
-  const onChangeSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.currentTarget.value);
-  };
-  const filteredFeatures = feature.filter(feature => feature.properties.name?.toLowerCase().includes(searchValue)
-                                                || feature.properties.description?.toLowerCase().includes(searchValue))
+    useEffect(() => {
+        dispatch(getPlacemarksTC(search))
+    }, [dispatch, search])
 
-  return (
-    <div className={s.app}>
-      <div className={s.search_container}>
-        {/*search component*/}
-        <div className={s.search}>
-          <input
-            type="text"
-            value={searchValue}
-            onChange={onChangeSearchValue}
-            className={s.search_box}
-            placeholder="Search"
-          />
-          <input value="X" type="submit" className={s.button} />
+    const handleMyLocation = () => {
+        const location = [latitude!, longitude!]
+        dispatch(myLocationAC(location, 14))
+        dispatch(centerPositionAC(location))
+    };
+
+    const onSubmitSearch = () => {
+        dispatch(getPlacemarksTC(searchValue))
+        setHideResults(true)
+    }
+
+    const onChangeSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value)
+    }
+
+    return (
+        <div className={s.app}>
+
+            <div className={s.search_container}>
+
+                {/*search component*/}
+                <div className={s.search}>
+                    <input type="text"
+                           value={searchValue}
+                           onChange={onChangeSearchValue}
+                           className={s.search_box}
+                           placeholder="Search"/>
+                    <input value="search"
+                           type="submit"
+                           onClick={onSubmitSearch}
+                           className={s.button}/>
+                    <input value="X"
+                           type="submit"
+                           className={s.button}/>
+                    {hideResults
+                        ? <button onClick={() => setHideResults(false)}>hide</button>
+                        : <button onClick={() => setHideResults(true)}>show</button>}
+                </div>
+
+                {hideResults && <div className={s.results}>
+                    <SearchBarInMap resultArray={feature} />
+                </div>}
+
+            </div>
+
+            <YMaps query={{lang: 'ru_RU', load: 'package.full'}}>
+                <Map width="100%"
+                     height="100vh"
+
+                     state={{zoom: zoom, center: center}}
+                     defaultState={mapState}
+                     modules={["geoObject.addon.editor", "geolocation", "geocode"]}>
+                    {feature.map((c) => {
+                        return <Placemark
+                            geometry={[c.geometry.coordinates[1], c.geometry.coordinates[0]]}
+                            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+                            properties={{
+                                balloonContentHeader: c.properties.CompanyMetaData.name,
+                                balloonContentBody: c.properties.CompanyMetaData.address,
+                                balloonContentFooter: c.properties.CompanyMetaData.url,
+                            }}
+                        />
+                    })}
+                    <Placemark
+                        geometry={[53.917485, 27.604842]}
+                        options={{
+                            preset: "islands#circleDotIcon",
+                            iconColor: '#002222'
+                        }}
+                        properties={{
+                            balloonContentHeader: "It-инкубатор",
+                            balloonContentBody: "Беларусь, Минск,ул. Сурганова, 2",
+                            balloonContentFooter: "http://it-kamasutra.com/",
+                        }}
+                    />
+                    <Placemark
+                        geometry={myLocation}
+
+                        options={{
+                            preset: "islands#circleDotIcon",
+                            iconColor: '#ff0000'
+                        }}
+                    />
+                    <GeolocationControl onClick={handleMyLocation} options={{float: 'right'}}/>
+                </Map>
+            </YMaps>
         </div>
+    )
+}
 
-        {searchValue.length > 0 && (
-          <div className={s.results}>
-            <SearchBarInMap resultArray={filteredFeatures} />
-          </div>
-        )}
-      </div>
-
-      <YMaps query={{ lang: "ru_RU", load: "package.full" }}>
-        <Map
-          width="100%"
-          height="100vh"
-          state={{ zoom: zoom, center: center }}
-          defaultState={mapState}
-          modules={["geoObject.addon.editor", "geolocation", "geocode"]}
-        >
-          {filteredFeatures.map(c => {
-            return (
-              <Placemark
-                geometry={[
-                  c.geometry.coordinates[1],
-                  c.geometry.coordinates[0]
-                ]}
-                modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
-                properties={{
-                  balloonContentHeader: c.properties.CompanyMetaData.name,
-                  balloonContentBody: c.properties.CompanyMetaData.address,
-                  balloonContentFooter: c.properties.CompanyMetaData.url
-                }}
-              />
-            );
-          })}
-          <Placemark
-            geometry={[53.917485, 27.604842]}
-            options={{
-              preset: "islands#circleDotIcon",
-              iconColor: "#002222"
-            }}
-            properties={{
-              balloonContentHeader: "It-инкубатор",
-              balloonContentBody: "Беларусь, Минск,ул. Сурганова, 2",
-              balloonContentFooter: "http://it-kamasutra.com/"
-            }}
-          />
-          <Placemark
-            geometry={myLocation}
-            options={{
-              preset: "islands#circleDotIcon",
-              iconColor: "#ff0000"
-            }}
-          />
-          <GeolocationControl
-            onClick={handleMyLocation}
-            options={{ float: "right" }}
-          />
-        </Map>
-      </YMaps>
-    </div>
-  );
-};
-
-export default MapApp;
+export default MapApp
--- src/store/MapReducer.tsx
+++ src/store/MapReducer.tsx
@@ -1,134 +1,122 @@
-import { MapsAPI } from "../api/yandex-map-api";
-import { Dispatch } from "redux";
+import {MapsAPI} from "../api/yandex-map-api";
+import {Dispatch} from 'redux';
 
-export type GetPlaceMarks = ReturnType<typeof getPlacemarksAC>;
-export type SearchType = ReturnType<typeof setSearchValueAC>;
-export type MyLocationType = ReturnType<typeof myLocationAC>;
-export type CenterPositionType = ReturnType<typeof centerPositionAC>;
+export type GetPlaceMarks = ReturnType<typeof getPlacemarksAC>
+export type SearchType = ReturnType<typeof setSearchValueAC>
+export type MyLocationType = ReturnType<typeof myLocationAC>
+export type CenterPositionType = ReturnType<typeof centerPositionAC>
 
-export type ActionsType =
-  | GetPlaceMarks
-  | SearchType
-  | MyLocationType
-  | CenterPositionType;
+export type ActionsType = GetPlaceMarks | SearchType | MyLocationType | CenterPositionType
 
 export type FeaturesType = {
-  geometry: CoordinatesType;
-  properties: PropertiesType;
-};
+    geometry: CoordinatesType
+    properties: PropertiesType
+}
 
 export type CoordinatesType = {
-  coordinates: Array<number>;
-};
+    coordinates: Array<number>
+}
 
 type PropertiesType = {
-  CompanyMetaData: CompanyMetaDataType;
-  name: string;
-  description: string;
-};
+    CompanyMetaData: CompanyMetaDataType
+}
 
 type CompanyMetaDataType = {
-  name: string;
-  address: string;
-  url: string;
-};
+    name: string
+    address: string
+    url: string
+}
 
 export type InitialStateType = {
-  feature: Array<FeaturesType>;
-  myLocation: Array<number>;
-  center: Array<number>;
-  zoom: number;
-  search: string;
-};
+    feature: Array<FeaturesType>
+    myLocation: Array<number>
+    center: Array<number>
+    zoom: number
+    search: string
+}
 
 let InitialState: InitialStateType = {
-  feature: [
-    {
-      geometry: {
-        coordinates: []
-      },
-      properties: {
-        CompanyMetaData: {
-          name: "",
-          address: "",
-          url: ""
-        },
-        name: "",
-        description: ""
-      }
-    }
-  ],
-  myLocation: [],
-  center: [32.99054220474171, 3.8141637443059158],
-  zoom: 3,
-  search: ""
-};
+    feature: [
+        {
+            geometry: {
+                coordinates: []
+            },
+            properties: {
+                CompanyMetaData: {
+                    name: '',
+                    address: '',
+                    url: ''
+                }
+            }
+        }
+    ],
+    myLocation: [],
+    center: [32.99054220474171, 3.8141637443059158],
+    zoom: 3,
+    search: ''
+}
 
 export const MapReducer = (state = InitialState, action: ActionsType) => {
-  switch (action.type) {
-    case "MAPS/GET_PLACEMARKS": {
-      return {
-        ...state,
-        ...state.feature,
-        feature: action.feature
-      };
-    }
-    case "MAPS/MY_LOCATION":
-      return {
-        ...state,
-        ...state.myLocation,
-        myLocation: action.location,
-        zoom: action.zoom
-      };
-    case "MAPS/CENTER_POSITION":
-      return {
-        ...state,
-        center: action.center
-      };
-    case "MAPS/SEARCH_VALUE":
-      return {
-        ...state,
-        searchValue: action.search
-      };
+    switch (action.type) {
+        case "MAPS/GET_PLACEMARKS": {
+            return {
+                ...state,
+                ...state.feature,
+                feature: action.feature
+            }
+        }
+        case "MAPS/MY_LOCATION":
+            return {
+                ...state,
+                ...state.myLocation,
+                myLocation: action.location,
+                zoom: action.zoom
+            }
+        case "MAPS/CENTER_POSITION":
+            return {
+                ...state,
+                center: action.center
+            }
+        case "MAPS/SEARCH_VALUE":
+            return {
+                ...state,
+                searchValue: action.search
+            }
 
-    default:
-      return state;
-  }
-};
+        default:
+            return state
+    }
+}
 
 export const getPlacemarksAC = (feature: Array<FeaturesType>) => {
-  return {
-    type: "MAPS/GET_PLACEMARKS",
-    feature
-  } as const;
-};
+    return {
+        type: 'MAPS/GET_PLACEMARKS', feature
+    } as const
+}
 
 export const setSearchValueAC = (search: string) => {
-  return {
-    type: "MAPS/SEARCH_VALUE",
-    search
-  } as const;
-};
+    return {
+        type: 'MAPS/SEARCH_VALUE', search
+    } as const
+}
 export const myLocationAC = (location: Array<number>, zoom: number) => {
-  return {
-    type: "MAPS/MY_LOCATION",
-    location,
-    zoom
-  } as const;
-};
+    return {
+        type: 'MAPS/MY_LOCATION', location, zoom
+    } as const
+}
 export const centerPositionAC = (center: Array<number>) => {
-  return {
-    type: "MAPS/CENTER_POSITION",
-    center
-  } as const;
-};
+    return {
+        type: 'MAPS/CENTER_POSITION', center
+    } as const
+}
 
 export const getPlacemarksTC = (search: string) => {
-  return (dispatch: Dispatch) => {
-    MapsAPI.getResult(search)
-      .then(res => {
-        dispatch(getPlacemarksAC(res.data.features));
-      })
-      .catch(e => {});
-  };
-};
+    return (dispatch: Dispatch) => {
+        debugger
+        MapsAPI.getResult(search).then(res => {
+            dispatch(getPlacemarksAC(res.data.features))
+        }).catch(e => {
+            }
+        )
+    }
+}
